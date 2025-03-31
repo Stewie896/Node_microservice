1e6947c26d2e39791829eb29c560c3acd6c6eb0e
Stewie896 <abhiralchhetri6@gmail.com>
2025-04-01 00:55:48 +0530

    Update config.js

--- post-service/cloudinary/config.js
+++ post-service/cloudinary/config.js
@@ -1,8 +1,8 @@
 const cloudinary = require('cloudinary').v2;
 
 module.exports = cloudinary.config({
-    cloud_name: 'dgmunrezl',
-    api_key: '595738788779629',
-    api_secret: 'jL2NmJyuulWXzW9PZsUtTVyJ1UM',
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY
+    api_secret: process.env.SEC_RET,
     secure: true
 })
